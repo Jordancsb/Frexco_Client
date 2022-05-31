c636ef98fb02712667b19aa1baeb3267620cc8a0
Jordancsb <jcaindoor@gmail.com>
2022-05-30 23:46:27 -0300

    chore: install bootstrap and create components folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Nav, NavLink } from 'react-bootstrap'
+import Home from './components/Home'
+import Sobre from './components/Sobre'
+import Produtos from './components/Produtos'
+import { BrowserRouter, Routes, Link, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+
+ 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Minha Aplicação React </h1>
+      <BrowserRouter>
+      
+      <Nav variant='tabs'>
+        <NavLink as={Link} to="/"> Página inicial </NavLink>
+        <NavLink as={Link} to="/produto"> Cadastro de Produtos </NavLink>
+        <NavLink as={Link} to="/produto"> Controle de Estoque </NavLink>
+        <NavLink as={Link} to="/sobre"> Sobre </NavLink>
+      </Nav>
+
+      <Routes>
+        <Route path='/' index element={ <Home/> }></Route>
+        <Route path='/produto' element={ <Produtos/> }></Route>
+        <Route path='/produto' element={ <Produtos/> }></Route>
+        <Route path='/sobre' element={ <Sobre/> }></Route>
+      </Routes>
+      
+      </BrowserRouter>
     </div>
   );
 }

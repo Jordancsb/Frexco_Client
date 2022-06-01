afa6399433838dfde05f98613ea265b6a0e00d59
Jordancsb <jcaindoor@gmail.com>
2022-06-01 00:09:49 -0300

    feat: forms and tables for finally methods

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Nav, NavLink } from 'react-bootstrap'
 import Home from './components/Home'
 import Sobre from './components/Sobre'
 import Produtos from './components/Produtos'
+
 import { BrowserRouter, Routes, Link, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './pages/Login/login'
+import Table from './components/Table'
+import Cart from './components/Cart'
 
-
- 
 function App() {
 
   return (
@@ -17,15 +19,17 @@ function App() {
       <Nav variant='tabs'>
         <NavLink as={Link} to="/"> Página inicial </NavLink>
         <NavLink as={Link} to="/produto"> Cadastro de Produtos </NavLink>
-        <NavLink as={Link} to="/produto"> Controle de Estoque </NavLink>
+        <NavLink as={Link} to="/login"> Controle de Estoque </NavLink>
         <NavLink as={Link} to="/sobre"> Sobre </NavLink>
       </Nav>
 
       <Routes>
         <Route path='/' index element={ <Home/> }></Route>
+        <Route path='/table' element={ <Table/> }></Route>
         <Route path='/produto' element={ <Produtos/> }></Route>
-        <Route path='/produto' element={ <Produtos/> }></Route>
+        <Route path='/login' element={ <Login/> }></Route>
         <Route path='/sobre' element={ <Sobre/> }></Route>
+        <Route path='/carrinho' index element={ <Cart/> }></Route>
       </Routes>
       
       </BrowserRouter>
--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -4,9 +4,10 @@ import { Table, Button, Form } from 'react-bootstrap'
 class Produtos extends React.Component {
 
   constructor(props){
-    super(props);
+    super(props)
 
     this.state = {
+        id: 0,
         nome: '',
         idade: '',
         produtos : []
@@ -40,22 +41,41 @@ class Produtos extends React.Component {
     })
   }
 
-   deleteProduct = (id) => {
-    fetch("http://localhost:8000/produto/" + id,
-    { method: 'DELETE',
-    headears: {'Content-Type': 'application/json'},
-    body: JSON.stringify(id)
-      })
+  updateProduto = (produto) => {
+    fetch("http://localhost:8000/produto", 
+      {method: 'PUT',
+      headears: {'Content-Type': 'application/json'},
+      body: JSON.stringify(produto)
+    })
     .then(resposta => {
-    if(resposta.ok){
+      if(resposta.ok){
         this.searchProduct()
-      } else{
+      }else{
         alert('Não foi possivel cadastrar o produto!')
       }
     })
   }
 
+  loadProduct = (id) => {
+    fetch("http://localhost:8000/produto/" + id, { method: 'GET'})
+    .then(resposta => resposta.json())
+    .then(produto => {
+      this.setState({ 
+        id : produto.id, 
+        nome: produto.nome,
+        idade: produto.idade
+      })
+    })
+  }
 
+   deleteProduct = (id) => {
+    fetch("http://localhost:8000/produto" + '/:id', { method: 'DELETE'})
+    .then(resposta => {
+    if(resposta.ok){
+        this.searchProduct()
+      }
+    })
+  }
 
   renderTable () {
     return <Table striped bordered hover>
@@ -67,15 +87,14 @@ class Produtos extends React.Component {
         </tr>
       </thead>
       <tbody>
-
       {
-        this.state.produtos.map((produto) =>
+        this.state.produtos.map((nome) =>
           <tr>
-            <td> { produto.nome } </td>
-            <td> { produto.idade } </td>
+            <td> { nome.nome } </td>
+            <td> { nome.idade } </td>
             <td> 
-            <Button variant="warning"> Editar </Button> 
-            <Button variant="danger" onClick={() => this.deleteProduct(produto.id)}> Excluir </Button>
+            <Button variant="warning"onClick={() => this.loadProduct(nome.id)}> Editar </Button> 
+            <Button variant="danger" onClick={() => this.deleteProduct(nome.id)}> Excluir </Button>
             </td>
           </tr>
         )
@@ -101,35 +120,60 @@ class Produtos extends React.Component {
   }
 
   submit = () => {
-    const produto = {
-      nome: this.state.nome,
-      idade: this.state.idade
+
+    if(this.state.id === 0){
+      const produto = {
+        nome: this.state.nome,
+        idade: this.state.idade
     }
     this.cadastrarProduto(produto)
-  }
 
-    render() {
+    }else{
+      const produto = {
+        id: this.state.id,
+        nome: this.state.nome,
+        idade: this.state.idade
+    }
+    this.UpdateProduto(produto)
+  }
+}
+
+reset = () => {
+  this.setState(
+    {
+      id: 0,
+      nome: '',
+      idade: ''
+    }
+  )
+}
+
+    render () {
       return (
         <>
           <Form>
           <Form.Group className="mb-3">
+            <Form.Label>ID</Form.Label>
+            <Form.Control placeholder="Codigo" value={this.state.id} readOnly={true}/>
+          </Form.Group>
+          <Form.Group className="mb-3">
             <Form.Label>Nome do Produto</Form.Label>
-            <Form.Control placeholder="Produto" value={this.state.nome} onChange={this.atualizaNome}/>
+            <Form.Control type='text' placeholder="Digite o Produto" value={this.state.nome} onChange={this.atualizaNome}/>
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label> Idade </Form.Label>
-            <Form.Control placeholder="Preço"value={this.state.idade} onChange={this.atualizaIdade}/>
+            <Form.Control type='number' placeholder="Preço"value={this.state.idade} onChange={this.atualizaIdade}/>
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label> Selecione a categoria </Form.Label>
           </Form.Group>
+          <Button variant='primary' type='submit' onClick={this.reset}> Novo </Button>
           <Button variant='primary' type='submit' onClick={this.submit}> Salvar </Button>
           </Form>
           {this.renderTable()}
         </>
       )
     }
+  }
 
-}
-  
-export default Produtos;
+export default Produtos
